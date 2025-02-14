ca204f7837f5b48e18d2bb043a1c389ade05de26
Anna Verkhovskaya <annaverpcs@gmail.com>
2025-02-13 18:53:11 -0800

    AnnaV:Session 19

---
+++ src/test/java/definitions/xPathLibrary.java
@@ -0,0 +1,15 @@
+package definitions;
+
+public class xPathLibrary {
+    public static String sMedicenterV1URL = "https://medicenter-qa1.vercel.app/";
+    public static String sMedicenterV2URL = "https://medicenter-qa2.vercel.app/";
+    public static String sGoogleURL = "https://www.google.com/";
+    public static String sLoginButton = "//button[contains(text(),'Login')]";
+    public static String sCreateAccountButton = "//button[contains(text(),'Create account')]";
+    public static String sSignInButton = "//button[@type='submit']";
+    public static String sEmailTextField = "//input[@id='email']";
+    public static String sPasswordTextField = "//input[@id='password']";
+
+
+}
+
--- src/test/resources/features/AnnaVSession18SeleniumWebDriver.feature
+++ src/test/resources/features/AnnaVSession18SeleniumWebDriver.feature
@@ -10,4 +10,6 @@ Feature: Medical Center Test scenarios using Selenium WebDriver methods
   And I wait for 1 sec
   Then AnnaV type "administrator1@gmail.com" to "Email" textfield
   Then AnnaV type "abc123" to "Password" textfield
+  And I wait for 1 sec
   And AnnaV click on "Sign In" button
+  And I wait for 2 sec
